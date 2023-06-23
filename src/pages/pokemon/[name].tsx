afebce1ace1453db626f47fb685625eebe6353f5
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-06-22 20:01:54 -0500

    feat(seo): add Head titles to pages.

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -10,6 +10,7 @@ import Image from 'next/image'
 import { PokemonLayout } from '@/layouts/pokemon/Pokemon'
 import { NextPageWithLayout } from '../_app'
 import { Pokemon } from '@/common/types'
+import Head from 'next/head'
 
 export const getServerSideProps: GetServerSideProps<{
   pokemon: Pokemon
@@ -31,62 +32,67 @@ const PokemonDetails: NextPageWithLayout<
   }, [isImgHovered, pokemon])
 
   return (
-    <section className="flex justify-center">
-      <div className="card glass w-96 shadow-2xl">
-        <figure className="relative h-64">
-          <Image
-            src={imgSrc}
-            className="scale-95 duration-300 ease-in-out hover:scale-100"
-            fill
-            alt="Picture of pokemon"
-            priority
-            onMouseEnter={() => setIsImgHovered(true)}
-            onMouseLeave={() => setIsImgHovered(false)}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title capitalize">{pokemon.name}</h2>
-          <div>
-            <p className="font-semibold">Abilities: </p>
-            <ul className="list-disc">
-              {pokemon.abilities.map(ability => (
-                <li className="ml-8" key={ability.ability.name}>
-                  {ability.ability.name}
-                </li>
-              ))}
-            </ul>
+    <>
+      <Head>
+        <title>{pokemon.name} Info</title>
+      </Head>
+      <section className="flex justify-center">
+        <div className="card glass w-96 shadow-2xl">
+          <figure className="relative h-64">
+            <Image
+              src={imgSrc}
+              className="scale-95 duration-300 ease-in-out hover:scale-100"
+              fill
+              alt="Picture of pokemon"
+              priority
+              onMouseEnter={() => setIsImgHovered(true)}
+              onMouseLeave={() => setIsImgHovered(false)}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title capitalize">{pokemon.name}</h2>
+            <div>
+              <p className="font-semibold">Abilities: </p>
+              <ul className="list-disc">
+                {pokemon.abilities.map(ability => (
+                  <li className="ml-8" key={ability.ability.name}>
+                    {ability.ability.name}
+                  </li>
+                ))}
+              </ul>
+
+              <br />
+
+              <p>
+                <strong>Base experience: </strong>
+                {pokemon.base_experience}
+              </p>
+
+              <br />
+
+              {/*Stats*/}
+              <p className="font-semibold">Stats: </p>
+              <ul className="list-disc">
+                {pokemon.stats.map(stat => (
+                  <li className="ml-8" key={stat.stat.name}>
+                    <strong>{stat.stat.name}:</strong> {stat.base_stat} -
+                    <strong>Effort:</strong> {stat.effort}
+                  </li>
+                ))}
+              </ul>
+            </div>
 
             <br />
 
-            <p>
-              <strong>Base experience: </strong>
-              {pokemon.base_experience}
-            </p>
-
-            <br />
-
-            {/*Stats*/}
-            <p className="font-semibold">Stats: </p>
-            <ul className="list-disc">
-              {pokemon.stats.map(stat => (
-                <li className="ml-8" key={stat.stat.name}>
-                  <strong>{stat.stat.name}:</strong> {stat.base_stat} -
-                  <strong>Effort:</strong> {stat.effort}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <br />
-
-          <div className="card-actions justify-center">
-            <Link href="/pokemon" className="btn-primary btn">
-              Volver
-            </Link>
+            <div className="card-actions justify-center">
+              <Link href="/pokemon" className="btn-primary btn">
+                Volver
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import { AuthLayout } from '@/layouts/auth/Auth'
 import type { ReactNode } from 'react'
@@ -5,33 +6,42 @@ import type { NextPageWithLayout } from '../_app'
 
 const SignInPage: NextPageWithLayout = () => {
   return (
-    <div className="card w-96 bg-neutral shadow-2xl shadow-neutral">
-      <div className="card-body gap-y-4">
-        <h2 className="card-title">Sign In</h2>
+    <>
+      <Head>
+        <title>Pokemon API Sign in</title>
+      </Head>
 
-        <input
-          type="text"
-          placeholder="Email "
-          className="input-bordered input w-full max-w-xs"
-        />
+      <div className="card w-96 bg-neutral shadow-2xl shadow-neutral">
+        <div className="card-body gap-y-4">
+          <h2 className="card-title">Sign In</h2>
 
-        <input
-          type="password"
-          placeholder="Password "
-          className="input-bordered input w-full max-w-xs"
-        />
+          <input
+            type="text"
+            placeholder="Email "
+            className="input-bordered input w-full max-w-xs"
+          />
 
-        <div className="card-actions flex-col items-center justify-center">
-          <button className="btn-primary btn-wide btn">Login</button>
-          <p className="text-center text-sm">
-            Don&apos;t have an account?{' '}
-            <Link href={'/sign-up'} className="text-secondary hover:underline">
-              Sing up here
-            </Link>
-          </p>
+          <input
+            type="password"
+            placeholder="Password "
+            className="input-bordered input w-full max-w-xs"
+          />
+
+          <div className="card-actions flex-col items-center justify-center">
+            <button className="btn-primary btn-wide btn">Login</button>
+            <p className="text-center text-sm">
+              Don&apos;t have an account?{' '}
+              <Link
+                href={'/sign-up'}
+                className="text-secondary hover:underline"
+              >
+                Sing up here
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
